8747ac93f0f2d9136e948fa1e0b3da6bb00703f2
Civi <civi@rcg.fun>
2026-01-16 20:47:46 +0800

    fix: unload mason

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -2,8 +2,8 @@
 vim.pack.add({
 	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
 	{ src = 'https://github.com/neovim/nvim-lspconfig' },
-	{ src = 'https://github.com/mason-org/mason.nvim' },
-	{ src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+	-- { src = 'https://github.com/mason-org/mason.nvim' },
+	-- { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
 	-- { src = 'https://github.com/j-hui/fidget.nvim' },
 	-- { src = 'https://github.com/vigoux/notifier.nvim' },
 	-- { src = 'https://github.com/Darazaki/indent-o-matic' },
