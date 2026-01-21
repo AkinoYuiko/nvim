4e356dc6c2b6068117a0ee1837429a7c2717d897
Civi <civi@rcg.fun>
2026-01-21 14:06:27 +0800

    refactor: init load plugin.init

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,21 +1,6 @@
--- Packages --
-vim.pack.add({
-	'https://github.com/neovim/nvim-lspconfig',
-	'https://github.com/mason-org/mason.nvim',
-	'https://github.com/mason-org/mason-lspconfig.nvim',
-	'https://github.com/j-hui/fidget.nvim',
-	-- 'https://github.com/folke/which-key.nvim',
-	-- 'https://github.com/nvim-tree/nvim-web-devicons',
-}, { confirm = false })
+require('plugin.snacks')
+require('plugin.everforest')
 require('plugin.treesitter')
--- mason
-require('mason').setup()
-require('mason-lspconfig').setup()
--- which-key
--- require('which-key').setup({ preset = 'modern' })
--- noice
--- require('plugin.noice')
--- mini.packs
 require('plugin.mini')
--- fidget
-require('fidget').setup({ notification = { override_vim_notify = true } })
+require('plugin.fidget')
+require('plugin.lsp')
