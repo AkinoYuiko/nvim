6656d5dcfe8a4d179511d82537ddf209ee52ac04
Civi <civi@rcg.fun>
2026-02-03 11:19:37 +0800

    refactor: many

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 vim.loader.enable()
 vim.g.mapleader = ' '
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 -- ext ui
@@ -11,7 +13,6 @@ opt.signcolumn = 'yes'
 opt.cursorline = true
 opt.swapfile = false
 opt.expandtab = true
-opt.shiftround = true
 opt.shiftwidth = 0
 opt.tabstop = 2
 opt.mouse = 'nv'
@@ -22,6 +23,7 @@ opt.pumwidth = 10
 opt.scrolloff = 8
 opt.sidescrolloff = 8
 opt.foldlevel = 99
+opt.foldmethod = 'expr'
 opt.fillchars = {
 	stl = ' ',
 	stlnc = '-',
@@ -34,7 +36,7 @@ opt.fillchars = {
 	eob = ' ',
 }
 opt.list = true
-opt.listchars = 'tab:» ,nbsp:+,trail:·,extends:→,precedes:←'
+opt.listchars = { tab = '» ', nbsp = '+', trail = '·', extends = '→', precedes = '←' }
 opt.linebreak = true
 opt.wrap = false
 -- opt.inccommand = 'split'
@@ -42,6 +44,8 @@ opt.ignorecase = true
 opt.smartcase = true
 opt.hlsearch = false
 opt.termguicolors = true
+-- netrw
+vim.g.netrw_banner = 0
 -- trans bg
 local groups = { 'Normal', 'NormalFloat', 'FloatBorder', 'SignColumn', 'LineNr' }
 for _, group in ipairs(groups) do
@@ -51,8 +55,8 @@ vim.cmd.colorscheme('everforest')
 -- function
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-local keymap = vim.keymap.set
 local group = augroup('momoGroup', { clear = true })
+local map = vim.keymap.set
 -- Custom Event: LazyFile
 autocmd({ 'BufReadPost', 'BufNewFile' }, {
 	desc = 'User Event LazyFile',
@@ -66,7 +70,7 @@ autocmd({ 'BufReadPost', 'BufNewFile' }, {
 	end,
 })
 -- Statusline
-autocmd('VimEnter', {
+autocmd('UIEnter', {
 	group = group,
 	once = true,
 	callback = function()
@@ -78,21 +82,26 @@ autocmd('User', {
 	group = group,
 	pattern = 'LazyFile',
 	callback = function()
-		vim.lsp.enable({ 'bashls', 'emmylua_ls', 'fish_lsp', 'jsonls', 'rust_analyzer', 'stylua', 'tombi', 'yamlls' })
-		vim.diagnostic.config({
-			virtual_text = true,
-			update_in_insert = true,
-			underline = true,
-			-- float = { border = 'rounded' },
+		vim.lsp.enable({
+			'bashls',
+			'emmylua_ls',
+			'stylua',
+			'fish_lsp',
+			'jsonls',
+			'rust_analyzer',
+			'tombi',
+			'yamlls',
 		})
+		vim.diagnostic.config({ virtual_text = true })
 		vim.filetype.add({ extension = { ['lsr'] = 'conf' } }) -- .lsr as .conf
-		keymap('n', '<leader>d', vim.diagnostic.open_float, { desc = 'diagnostic messages' })
-		keymap('n', '[d', function() vim.diagnostic.jump({ wrap = true, count = -1 }) end, { desc = 'prev diagnostic' })
-		keymap('n', ']d', function() vim.diagnostic.jump({ wrap = true, count = 1 }) end, { desc = 'next diagnostic' })
+		map('n', '<leader>d', vim.diagnostic.open_float, { desc = 'diagnostic messages' })
+		map('n', '[d', function() vim.diagnostic.jump({ wrap = true, count = -1 }) end, { desc = 'prev diagnostic' })
+		map('n', ']d', function() vim.diagnostic.jump({ wrap = true, count = 1 }) end, { desc = 'next diagnostic' })
 		-- lsp hover
-		keymap('n', '<leader>k', vim.lsp.buf.hover, { desc = 'lsp hover' })
+		map('n', '<leader>k', vim.lsp.buf.hover, { desc = 'lsp hover' })
 		-- Fast diagnostic
-		keymap({ 'n', 'x' }, 'gw', vim.lsp.buf.format, { desc = 'format' })
+		map({ 'n', 'x' }, 'gw', vim.lsp.buf.format, { desc = 'format' })
+		map({ 'n', 'x' }, 'gq', '<nop>', { noremap = true })
 	end,
 })
 -- Highlight Yanked Texts
@@ -130,64 +139,69 @@ autocmd('BufReadPost', {
 -- 		end
 -- 	end,
 -- })
--- keymap
-keymap('n', '<space>', '<Nop>', { noremap = true })
+-- map
+map('n', '<space>', '<Nop>', { noremap = true })
 -- fast command
-keymap({ 'n', 'o', 'x' }, ';', ':', { noremap = true })
+map({ 'n', 'o', 'x' }, ';', ':', { noremap = true })
 -- fast move to line begin/end
-keymap({ 'n', 'o', 'x' }, 'H', '^', { noremap = true })
-keymap({ 'n', 'o', 'x' }, 'L', 'g_', { noremap = true })
+map({ 'n', 'o', 'x' }, 'H', '^', { noremap = true })
+map({ 'n', 'o', 'x' }, 'L', 'g_', { noremap = true })
 -- fast save/quit and close buffer
-keymap('n', 'W', '<cmd>w<cr>', { noremap = true })
-keymap('n', 'Q', '<cmd>q<cr>', { noremap = true })
-keymap('n', 'B', '<cmd>bd<cr>', { noremap = true })
--- keymap('n','ca', '<cmd>silent %y+<cr>')
--- keymap('n', '<leader>c', '<cmd>set spell! spell?<cr>' )
-keymap('n', '<leader>l', '<cmd>set list! list?<cr>', { noremap = true })
-keymap('n', '<leader>m', '<cmd>set number! number?<cr>', { noremap = true })
-keymap('n', '<leader>w', '<cmd>set wrap! wrap?<cr>', { noremap = true })
-keymap('n', '<leader><cr>', '<cmd>noh<cr>', { noremap = true })
--- keymap('n','<leader><leader>', '/<++><CR>:noh<CR>"_c4l',, {noremap=true})
-keymap('n', 'j', 'gj', { noremap = true })
-keymap('n', 'k', 'gk', { noremap = true })
-keymap('n', 'J', '<c-d>', { noremap = true })
-keymap('n', 'K', '<c-u>', { noremap = true })
+map('n', 'W', '<cmd>w<cr>', { noremap = true })
+map('n', 'Q', '<cmd>q<cr>', { noremap = true })
+map('n', 'B', '<cmd>bd<cr>', { noremap = true })
+-- map('n','ca', '<cmd>silent %y+<cr>')
+-- file explorer and tab to switch buffers
+map('n', '<leader>e', '<cmd>Lexplore<cr>', { noremap = true })
+map('n', '<leader>b', '<cmd>buffers<cr>', { noremap = true })
+map('n', '<tab>', '<cmd>bNext<cr>', { noremap = true })
+map('n', '<S-tab>', '<cmd>bprevious<cr>', { noremap = true })
+-- map('n', '<leader>c', '<cmd>set spell! spell?<cr>' )
+map('n', '<leader>l', '<cmd>set list! list?<cr>', { noremap = true })
+map('n', '<leader>m', '<cmd>set number! number?<cr>', { noremap = true })
+map('n', '<leader>w', '<cmd>set wrap! wrap?<cr>', { noremap = true })
+map('n', '<leader><cr>', '<cmd>noh<cr>', { noremap = true })
+-- map('n','<leader><leader>', '/<++><CR>:noh<CR>"_c4l',, {noremap=true})
+map('n', 'j', 'gj', { noremap = true })
+map('n', 'k', 'gk', { noremap = true })
+map('n', 'J', '<c-d>', { noremap = true })
+map('n', 'K', '<c-u>', { noremap = true })
 -- window
-keymap('n', '<C-h>', '<C-w>h', { noremap = true })
-keymap('n', '<C-j>', '<C-w>j', { noremap = true })
-keymap('n', '<C-k>', '<C-w>k', { noremap = true })
-keymap('n', '<C-l>', '<C-w>l', { noremap = true })
+map('n', '<C-h>', '<C-w>h', { noremap = true })
+map('n', '<C-j>', '<C-w>j', { noremap = true })
+map('n', '<C-k>', '<C-w>k', { noremap = true })
+map('n', '<C-l>', '<C-w>l', { noremap = true })
 -- Keep current search result centered on the screen
--- keymap('n', 'n', 'nzz' , {noremap=true})
--- keymap('n', 'N', 'Nzz' , {noremap=true})
+-- map('n', 'n', 'nzz' , {noremap=true})
+-- map('n', 'N', 'Nzz' , {noremap=true})
 -- stay in visual after <,>
--- keymap('v', '<', '<gv', {noremap=true})
--- keymap('v', '>', '>gv', {noremap=true})
--- keymap('n', '<leader>W', '<c-w>w' , {noremap=true})
-keymap('n', '<leader>sh', '<cmd>set nosplitright | vsplit<cr>', { noremap = true })
-keymap('n', '<leader>sj', '<cmd>set splitbelow | split<cr>', { noremap = true })
-keymap('n', '<leader>sk', '<cmd>set nosplitbelow | split<cr>', { noremap = true })
-keymap('n', '<leader>sl', '<cmd>set splitright | vsplit<cr>', { noremap = true })
-keymap('n', '<leader>smv', '<c-w>t<c-W>H', { noremap = true })
-keymap('n', '<leader>smh', '<c-w>t<c-W>K', { noremap = true })
+map('v', '<', '<gv', { noremap = true })
+map('v', '>', '>gv', { noremap = true })
+-- map('n', '<leader>W', '<c-w>w' , {noremap=true})
+map('n', '<leader>sh', '<cmd>set nosplitright | vsplit<cr>', { noremap = true })
+map('n', '<leader>sj', '<cmd>set splitbelow | split<cr>', { noremap = true })
+map('n', '<leader>sk', '<cmd>set nosplitbelow | split<cr>', { noremap = true })
+map('n', '<leader>sl', '<cmd>set splitright | vsplit<cr>', { noremap = true })
+map('n', '<leader>smv', '<c-w>t<c-W>H', { noremap = true })
+map('n', '<leader>smh', '<c-w>t<c-W>K', { noremap = true })
 -- open vim config
--- keymap('n', '<leader>vim', '<cmd>edit ' .. vim.fn.stdpath('config') .. '/init.lua | Chdir silent<cr>' , {noremap=true})
+map('n', '<leader>vim', '<cmd>cd ' .. vim.fn.stdpath('config') .. '| edit init.lua<cr>', { noremap = true })
 -- update all packs
--- keymap('n', '<leader>up', function() vim.pack.update(nil, { force = true }) end , {noremap=true})
+-- map('n', '<leader>up', function() vim.pack.update(nil, { force = true }) end , {noremap=true})
 -- Line Move
-keymap('v', 'J', ":m '>+1<cr>gv=gv", { silent = true, noremap = true })
-keymap('v', 'K', ":m '<-2<cr>gv=gv", { silent = true, noremap = true })
+map('v', 'J', ":m '>+1<cr>gv=gv", { silent = true, noremap = true })
+map('v', 'K', ":m '<-2<cr>gv=gv", { silent = true, noremap = true })
 -- fast Norm in visual
-keymap('v', 'N', ':norm ', { noremap = true })
+map('v', 'N', ':norm ', { noremap = true })
 -- systemd-wide yank, cut and paste
-keymap('v', '<leader>c', '"+y', { noremap = true })
-keymap('v', '<leader>x', '"+d', { noremap = true })
-keymap('v', '<leader>p', '"+p', { noremap = true })
+map('v', '<leader>c', '"+y', { noremap = true })
+map('v', '<leader>x', '"+d', { noremap = true })
+map('v', '<leader>p', '"+p', { noremap = true })
 -- fast switch mode in terminal
-keymap('t', '<c-n>', '<C-\\><C-N>', { noremap = true })
-keymap('t', '<c-o>', '<C-\\><C-N><C-O>', { noremap = true })
+map('t', '<c-n>', '<C-\\><C-N>', { noremap = true })
+map('t', '<c-o>', '<C-\\><C-N><C-O>', { noremap = true })
 -- Ctrl + h,j,k,l to mode cursor in insert/command mode
-keymap({ 'i', 'c' }, '<c-h>', '<Left>', { noremap = true })
-keymap({ 'i', 'c' }, '<c-l>', '<Right>', { noremap = true })
-keymap({ 'i', 'c' }, '<c-a>', '<Home>', { noremap = true })
-keymap({ 'i', 'c' }, '<c-e>', '<End>', { noremap = true })
+map({ 'i', 'c' }, '<c-h>', '<Left>', { noremap = true })
+map({ 'i', 'c' }, '<c-l>', '<Right>', { noremap = true })
+map({ 'i', 'c' }, '<c-a>', '<Home>', { noremap = true })
+map({ 'i', 'c' }, '<c-e>', '<End>', { noremap = true })
