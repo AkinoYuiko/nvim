0e4d1c8e118bff30195bd24a9b552a6bc1c85aa0
Civi <civi@rcg.fun>
2026-02-03 22:59:57 +0800

    feat: disable format for emmylua_ls completely
    we use stylua

---
+++ lsp/emmylua_ls.lua
@@ -0,0 +1,12 @@
+---@type vim.lsp.Config
+return {
+	cmd = { 'emmylua_ls' },
+	filetypes = { 'lua' },
+	root_markers = { '.emmyrc.json', '.luarc.json', '.luacheckrc', '.git' },
+	workspace_required = false,
+	on_init = momo.lua_ls_on_init,
+	on_attach = function(client)
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+	end,
+}
