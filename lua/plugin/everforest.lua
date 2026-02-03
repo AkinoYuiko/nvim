474253e85a8302d5e8a0dce3c3ad48e4e3f54224
Civi <civi@rcg.fun>
2026-02-03 12:15:39 +0800

    refactor: improve boost performance

--- lua/plugin/everforest.lua
+++ lua/plugin/everforest.lua
@@ -1,6 +1,12 @@
-vim.pack.add({ 'https://github.com/sainnhe/everforest' }, { confirm = false })
--- Colorscheme settings
-vim.g.everforest_background = 'hard'
-vim.g.everforest_float_style = 'blend'
-vim.g.everforest_transparent_background = 2
-vim.cmd.colorscheme('everforest')
+local groups = { 'Normal', 'NormalFloat', 'FloatBorder', 'SignColumn', 'LineNr' }
+for _, group in ipairs(groups) do
+	vim.api.nvim_set_hl(0, group, { bg = 'none' })
+end
+vim.schedule(function()
+	vim.pack.add({ 'https://github.com/sainnhe/everforest' }, { confirm = false })
+	-- Colorscheme settings
+	vim.g.everforest_background = 'hard'
+	vim.g.everforest_float_style = 'blend'
+	vim.g.everforest_transparent_background = 2
+	vim.cmd.colorscheme('everforest')
+end)
