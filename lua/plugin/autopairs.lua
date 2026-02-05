bd1d1cf01669dee06bbd83eea42ee9d2f25ad756
Civi <civi@rcg.fun>
2026-02-05 16:54:43 +0800

    feat: autopairs check tree-sitter

--- lua/plugin/autopairs.lua
+++ lua/plugin/autopairs.lua
@@ -5,7 +5,14 @@ vim.pack.add({ 'https://github.com/windwp/nvim-autopairs' }, {
 			once = true,
 			callback = function()
 				vim.cmd.packadd(pack.spec.name)
-				vim.schedule(function() require('nvim-autopairs').setup({}) end)
+				vim.schedule(
+					function()
+						require('nvim-autopairs').setup({
+							check_ts = true,
+							enable_check_bracket_line = true,
+						})
+					end
+				)
 			end,
 		})
 	end,
