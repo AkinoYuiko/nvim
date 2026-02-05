eba332a20dc476614b9c71a04fd9ac2a10532e45
Civi <civi@rcg.fun>
2026-02-05 16:55:34 +0800

    refactor: change time to load

--- lua/plugin/conform.lua
+++ lua/plugin/conform.lua
@@ -9,15 +9,20 @@ local opts = {
 		injected = { options = { ignore_errors = true } },
 	},
 }
-vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
-	group = vim.api.nvim_create_augroup('lsp.conform', { clear = true }),
-	once = true,
-	callback = function()
-		vim.schedule(function()
-			vim.pack.add({ 'https://github.com/stevearc/conform.nvim' }, { confirm = false })
-			---@diagnostic disable-next-line
-			require('conform').setup(opts)
-			vim.keymap.set({ 'n', 'x' }, 'gw', function() require('conform').format({ lsp_fallback = true }) end)
-		end)
+vim.pack.add({ 'https://github.com/stevearc/conform.nvim' }, {
+	confirm = false,
+	load = function(plug_data)
+		vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
+			group = vim.api.nvim_create_augroup('lsp.conform', { clear = true }),
+			once = true,
+			callback = function()
+				vim.cmd.packadd(plug_data.spec.name)
+				vim.schedule(function()
+					---@diagnostic disable-next-line
+					require('conform').setup(opts)
+					vim.keymap.set({ 'n', 'x' }, 'gw', function() require('conform').format({ lsp_fallback = true }) end)
+				end)
+			end,
+		})
 	end,
 })
