e5e94a0dd38c497c99ce26bc3317a946a08e4afa
Civi <civi@rcg.fun>
2026-02-07 01:50:19 +0800

    fix: module rename
    turn require"vim._extui".enable{} to require"vim._core.ui2".enable{}

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = ' '
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 -- experimental feat: ext_ui
-require('vim._extui').enable({})
+require('vim._core.ui2').enable({})
 require('core.options')
 
 _G.momo = require('util')
