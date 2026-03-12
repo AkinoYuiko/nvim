543c03d3cd94fcc85e12041293794a0184b72cf2
Civi <civi@rcg.fun>
2026-03-12 23:05:24 +0800

    test: add source file to add packs

---
+++ test/packs.lua
@@ -0,0 +1,12 @@
+if vim.fn.has('nvim-0.12') == 0 then return end
+vim.pack.add({
+	'https://github.com/stevearc/conform.nvim',
+	'https://github.com/sainnhe/everforest',
+	-- 'https://github.com/j-hui/fidget.nvim',
+	'https://github.com/folke/flash.nvim',
+	'https://github.com/nvim-mini/mini.nvim',
+	'https://github.com/windwp/nvim-autopairs',
+	'https://github.com/neovim/nvim-lspconfig',
+	'https://github.com/nvim-treesitter/nvim-treesitter',
+	'https://github.com/folke/snacks.nvim',
+})
