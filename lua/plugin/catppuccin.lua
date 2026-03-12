d06d21bd52a85b360835857da9fe51f7923cf9f8
Civi <civi@rcg.fun>
2026-03-12 14:48:22 +0800

    refactor: colorscheme

--- lua/plugin/catppuccin.lua
+++
@@ -1,8 +0,0 @@
-vim.pack.add({ { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' } }, { confirm = false })
-require('catppuccin').setup({
-	transparent_background = true,
-	float = {
-		transparent = true,
-	},
-})
-vim.cmd.colorscheme('catppuccin')
--- lua/plugin/everforest.lua
+++
@@ -1,5 +0,0 @@
--- Colorscheme settings
-vim.g.everforest_background = 'hard'
-vim.g.everforest_float_style = 'blend'
-vim.g.everforest_transparent_background = 2
-vim.cmd.colorscheme('everforest')
